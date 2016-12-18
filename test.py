a8049d0073c8cb5ea482a9394c423c930cf71142
Pterosaur <ganze718@gmail.com>
2016-12-19 00:05:46 +0800

    add test file

---
+++ test.py
@@ -0,0 +1,42 @@
+
+from PyLuaTblParser import *
+
+parser = PyLuaTblParser()
+
+parser.load('''{
+    array = {65,23,5,},
+    dict = {
+        mixed = {
+              
+              43,54.33,false,9,string = "value",
+            },
+            null = nil,
+            array = {3,6,4,},
+            string = "value",
+    },
+}''')
+print parser.dumpDict()
+print parser.dumpLuaTable("table_test.lua")
+# print PythonStrToStruct('''{
+#      "array": [65, 23, 5],
+#      "dict": {
+#           "mixed": {
+#                1: 43,
+#                2: 54.33,
+#                3: False,
+#                4: 9,
+#                "string": "value"
+#           },
+#           "null" : None,
+#           "array": [3, 6,4],
+#           "string": "value"
+#      }
+# }
+# ''')[1].ToLuaStr()
+parser.loadLuaTable("table_test.lua")
+
+print parser["array"]
+parser["array"] = {"test_set_item":True}
+print parser.dump()
+parser.update({"test_update":[1,2]})
+print parser.dump()
