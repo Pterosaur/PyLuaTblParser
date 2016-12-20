e79b03138a9689114d6a70440628ad2bea4de2ff
Pterosaur <ganze718@gmail.com>
2016-12-20 11:55:13 +0800

    load lua str case_1

--- PyLuaTblParser.py
+++ PyLuaTblParser.py
@@ -1,18 +1,20 @@
-import track_map
-
-source_input = ""
-
-class NonePattern(Exception):
-    def __init__(self, arg):
-        self.__arg = arg
-    def __str__(self):
-        return self.__arg
+from common import *
+# class NonePattern(Exception):
+#     def __init__(self, arg):
+#         self.__arg = arg
+#     def __str__(self):
+#         return self.__arg
 
 class SpaceFilter:
     space_set = set([' ','\t','\n', '\r'])
     def __call__(self, s, pos = 0):
-        while pos < len(s) and s[pos] in SpaceFilter.space_set:
-            pos+=1
+        while pos < len(s):
+            # if pos + 1 < len(s) and s[pos:pos+2] in self.space_set :
+                # pos += 2
+            if s[pos] in self.space_set:
+                pos += 1
+            else:
+                break
         return pos
 
 def __space_filter(func):
@@ -25,6 +27,13 @@ def __space_filter(func):
             return pos + p, t
     wrapper.__name__ = func.__name__
     return wrapper
+
+def LuaInvalidFilter(s = ""):
+    p  = SpaceFilter()(s)
+    p +=CommentFilter().FromLuaStr(s[p:])
+    p += SpaceFilter()(s[p:])
+    return p
+
 
 class Number:
     def __init__(self,data = 0,key = None): 
@@ -43,6 +52,7 @@ class NumberDelimiter:
         is_float = False
         is_number = False
         is_hex = False
+        is_scientfic = False
         while pos < len(s):
             if s[pos].isdigit():
                 is_number = True
@@ -55,6 +65,12 @@ class NumberDelimiter:
                 elif s[pos] == '.' and is_float == False:
                     is_float = True
                     pos += 1
+                #e
+                elif is_scientfic == False and (s[pos] == 'e' or s[pos] == 'E'):
+                    is_scientfic = True
+                    pos += 1
+                    if pos < len(s) and (s[pos] == '-' or s[pos] == '+'):
+                        pos += 1
                 else:
                     break
         if is_number == False:
@@ -86,8 +102,10 @@ class StringDelimiter:
         end_char = s[pos] 
         pos+=1
         while pos < len(s):
-            if s[pos] == end_char:
-                if s[pos - 1] != '\\':
+            if s[pos] == '\\':
+                pos += 2
+                continue
+            elif s[pos] == end_char:
                     #+1 because of the skipping of the end char --"' or "", for next parser
                     return pos + 1, String(s[1:pos], end_char)        
             pos += 1
@@ -142,6 +160,107 @@ class NullDelimiter:
     def FromLuaStr(self, s = ""):
         return self.__FromStr(s,"nil")
 
+# class Comment:
+#     def __init__(self, s, f = "--%s\n"):
+#         self.__data = s
+#         self.__lua_format = f
+#     def ToPythonStr(self):
+#         return "#%s\n" % self.__data
+#     def ToLuaStr(self):
+#         return self.__lua_format % self.__data
+
+class CommentFilter:
+    def __FromStr(self, s = "", b = "", e = ""):
+        if s[:len(b)] != b:
+            return 0
+        pos = s.find(e)
+        if pos == -1:
+            return 0
+        return pos + len(e)#, Comment(s[:pos + len(e)], b+"%s"+e) 
+    def FromPythonStr(self, s = ""):
+        p = self.__FromStr(s, "#", "\n")
+        if p == 0:
+            return p
+        return p + self.FromLuaStr(s[p:])
+    def FromLuaStr(self, s = ""):
+        p = self.__FromStr(s, "--[[","]]")
+        if p == 0:
+            p = self.__FromStr(s, "--", "\n")
+            if p == 0:
+                return p
+        return p + LuaInvalidFilter(s[p:])
+
+        
+# class Key:
+#     def __init__(self, k):
+#         self.__data = k
+#     def ToPythonStr(self):
+#         if isinstance(self.__data, int):
+#             return str(self.__data)
+#         elif isinstance(self.__data, int):
+#             return '"%s"' % self.__data
+#         else:
+#             raise NonePattern(str(self.__data))
+#     def ToLuaStr(self):
+#         if isinstance(self.__data, int):
+#             return "[%d]" % self.__data
+#         elif isinstance(self.__data, str):
+#             return '["%s"]' % self.__data
+#         else:
+#             raise NonePattern(str(self.__data))
+
+class KeyDelimiter:
+    def FromPythonStr(self, s = ""):
+        pos = SpaceFilter()(s)
+        if len(s[pos:]) == 0:
+            return 0
+        p,v = NumberDelimiter().FromPythonStr(s[pos:])
+        if p == 0:
+            p,v = StringDelimiter().FromPythonStr(s[pos:])
+            if p == 0:
+                return 0, None
+        pos += p
+        pos += SpaceFilter()(s[pos:])
+        if s[pos] != ':':
+            return 0, None
+        pos += 1
+        return pos, v.GetRawData()
+    def FromLuaStr(self, s = ""):
+        pos = SpaceFilter()(s)
+        if len(s[pos:]) == 0:
+            return 0, None
+        if s[pos] == '[':
+            pos += 1
+            #filtrate space and comment
+            pos += LuaInvalidFilter(s[pos:])
+            p, v = NumberDelimiter().FromLuaStr(s[pos:])
+            if p == 0:
+                p, v = StringDelimiter().FromLuaStr(s[pos:]) 
+                if p == 0:
+                    return 0, None
+            pos += p 
+            #filtrate space and comment
+            pos += LuaInvalidFilter(s[pos:])
+            if s[pos] == ']':
+                pos += 1
+                pos += LuaInvalidFilter(s[pos:])
+                if s[pos] == '=':
+                    pos += 1
+                    return pos, v.GetRawData()
+            return 0, None
+        if s[pos].isalpha() == False and s[pos] != '_':
+            return 0, None
+        key_buffer = ""
+        while s[pos].isalpha() or s[pos].isdigit() or s[pos] == '_':
+            key_buffer += s[pos]
+            pos += 1
+            if pos >= len(s):
+                return 0, None
+        #filtrate space and comment
+        pos += LuaInvalidFilter(s[pos:])
+        if s[pos] != '=':
+            return 0, None
+        return pos+1,key_buffer
 
 def Traits(value):
     # if isinstance(value, int) or isinstance(value, float) or isinstance(value, long):
@@ -194,15 +313,16 @@ class List:
         self.__data[key] = Traits(value)
 
 class ListDelimiter:
-    def __FromStr(self, s , begin_char, end_char, delimiter_char, generator):
+    def __FromStr(self, s , begin_char, end_char, delimiter_char, generator, comment_point):
         pos = 0
         l = []
         if s[pos] != begin_char:
             return 0,None
         pos += 1
         delimiter_emerge = True
+        pos +=comment_point(s[pos:])
         while pos < len(s):
-
+            pos += SpaceFilter()(s[pos:])
             if s[pos] == end_char: 
                 #+1 because of the skipping of end char --"]", for next parser
                 return pos + 1, List(l)
@@ -210,8 +330,6 @@ class ListDelimiter:
                 if delimiter_emerge == True:
                     break
                 delimiter_emerge = True
-                pos += 1
-            elif s[pos] in SpaceFilter.space_set:
                 pos += 1
             else:
                 if delimiter_emerge == False:
@@ -222,14 +340,15 @@ class ListDelimiter:
                     break
                 pos += p
                 l.append(t)
+        pos +=comment_point(s[pos:])
         return 0, None
 
     def FromPythonStr(self, s = ""):
-        return self.__FromStr(s, '[',']',',',PythonStrToStruct)
+        return self.__FromStr(s, '[',']',',',PythonStrToStruct, CommentFilter().FromPythonStr)
 
     def FromLuaStr(self, s = ""):
         try:
-            return self.__FromStr(s, '{','}',',',LuaStrToStruct)
+            return self.__FromStr(s, '{','}',',',LuaStrToStruct,CommentFilter().FromLuaStr)
         except NonePattern:
             return 0, None
 
@@ -240,17 +359,17 @@ def AddPythonPrefix(key, value):
     elif isinstance(key, str):
         buffer += "\"" + key + "\""
     else:
-        raise "KeyError"
+        raise NonePattern(str(key) + "=" +value)
     buffer += ":"
     return buffer + value.ToPythonStr()
 
 def AddLuaPrefix(key, value):
-    # key = str(key)
-    # if isinstance(value.key, str):
     if isinstance(key, str):
-        return str(key) + "=" + value.ToLuaStr()
+        return ('["%s"]' % key) + "=" + value.ToLuaStr()
+    elif isinstance(key, int):
+        return ('[%d]' % key )+ "=" + value.ToLuaStr()
     else:
-        return value.ToLuaStr()
+        raise NonePattern(str(key) + "=" +value)
 
 class Dict:
     def __init__(self, d = {}):
@@ -287,6 +406,7 @@ class DictDelimiter:
         pos += 1
         delimiter_emerge = True
         while pos < len(s):
+            pos += SpaceFilter()(s[pos:])
             if s[pos] == "}":
                 return pos + 1, Dict(d)
             elif s[pos] == ',' :
@@ -294,14 +414,13 @@ class DictDelimiter:
                     break
                 delimiter_emerge = True
                 pos += 1
-            elif s[pos] in SpaceFilter.space_set:
-                pos += 1
             else:
                 if delimiter_emerge == False:
                     break
                 delimiter_emerge = False
                 #get key
                 p, k = PythonStrToStruct(s[pos:])
+                p, k = KeyDelimiter()()
                 if p == 0:
                     break
                 else:
@@ -327,14 +446,14 @@ class DictDelimiter:
             return 0, None
         pos += 1
         while pos < len(s):
+            # pos += SpaceFilter()(s[pos:])
+            pos += LuaInvalidFilter(s[pos:])
             if s[pos] == "}":
                 return pos + 1, Dict(d)
             elif s[pos] == ',':
                 if delimiter_emerge == True:
                     break
                 delimiter_emerge = True
-                pos += 1
-            elif s[pos] in SpaceFilter.space_set:
                 pos += 1
             else:
                 if delimiter_emerge == False:
@@ -348,29 +467,41 @@ class DictDelimiter:
                 except NonePattern:
                     #get key
                     #filtrate the space
-                    pos += SpaceFilter()(s[pos:])
-                    if pos == len(s):
+                    # pos += SpaceFilter()(s[pos:])
+                    # if pos == len(s):
+                    #     break
+                    # #filtrate the Comment
+                    # p,v = CommentDelimiter().FromLuaStr(s[pos:])
+                    # if p != 0:
+                    #     pos += p
+                    # p = SpaceFilter()(s[pos:])
+                    # if p!= 0:
+                    #     pos +=p
+                    # #right flag
+                    # if pos > len(s) or s[pos].isalpha() == False:
+                    #     break
+                    # key_buffer = "" 
+                    # while s[pos] != '=' and s[pos] not in SpaceFilter.space_set:
+                    #     key_buffer += s[pos] 
+                    #     pos += 1
+                    #     if pos == len(s):
+                    #         return 0, None
+                    # pos += SpaceFilter()(s[pos:])
+                    # if pos == len(s):
+                    #     break
+                    # if s[pos] != '=':
+                    #     break
+                    # pos += 1
+                    p, k = KeyDelimiter().FromLuaStr(s[pos:])
+                    if p == 0:
                         break
-                    if s[pos].isalpha() == False:
-                        break
-                    key_buffer = "" 
-                    while s[pos] != '=' and s[pos] not in SpaceFilter.space_set:
-                        key_buffer += s[pos] 
-                        pos += 1
-                        if pos == len(s):
-                            return 0, None
-                    pos += SpaceFilter()(s[pos:])
-                    if pos == len(s):
-                        break
-                    if s[pos] != '=':
-                        break
-                    pos += 1
+                    pos += p
                     p, v = LuaStrToStruct(s[pos:])
                     if p == 0:
                         break
                     else:
                         pos += p
-                        d[key_buffer] = v
+                        d[k] = v
         return 0, None
 
 class DelimiterAdaptor:
@@ -381,51 +512,79 @@ class DelimiterAdaptor:
         return self.__delimiter(self.__arg)
     def GetArg(self):
         return self.__arg
+    def SetArgOffset(self,offset):
+        if offset < len(self.__arg):
+            self.__arg = self.__arg[offset:]
+        else:
+            self.__arg = self.__arg[len(self.__arg) - 1:]
 
+import track_map
+source_input = ""
 def Generator(*ds):
     for d in ds:
         pos, struct = d()
         if pos != 0:
             return pos, struct
-    # raise NonePattern(ds[0].GetArg())
-    global source_input
-    track_map.ascii(source_input+ "\n--------------\n" +ds[0].GetArg())
+    raise NonePattern(ds[0].GetArg())
+    # global source_input
+    # track_map.ascii(source_input+ "\n--------------\n" +ds[0].GetArg(), 10000)
+    # track_map.ascii(ds[0].GetArg(), 10000)
 
 @__space_filter
 def PythonStrToStruct(s):
+    p = CommentFilter().FromPythonStr(s)
+    p += SpaceFilter()(s[p:])
+    s = s[p:]
     number_generator = DelimiterAdaptor(s, NumberDelimiter().FromPythonStr)
     string_generator = DelimiterAdaptor(s, StringDelimiter().FromPythonStr)
     bool_generator   = DelimiterAdaptor(s, BoolDelimiter().FromPythonStr)
     null_generator   = DelimiterAdaptor(s, NullDelimiter().FromPythonStr)
+    # comment_generator= DelimiterAdaptor(s, CommentDelimiter().FromPythonStr)
     list_generator   = DelimiterAdaptor(s, ListDelimiter().FromPythonStr)
     dict_generator   = DelimiterAdaptor(s, DictDelimiter().FromPythonStr)
-    return Generator(number_generator, string_generator, bool_generator, null_generator, list_generator,dict_generator)
+    return Generator(
+        number_generator,
+        string_generator, 
+        bool_generator,
+        null_generator,
+        # comment_generator,
+        list_generator,
+        dict_generator
+     )
 
 @__space_filter
 def LuaStrToStruct(s):
+    p = CommentFilter().FromLuaStr(s)
+    p += SpaceFilter()(s[p:])
+    s = s[p:]
     number_generator = DelimiterAdaptor(s, NumberDelimiter().FromLuaStr)
     string_generator = DelimiterAdaptor(s, StringDelimiter().FromLuaStr)
     bool_generator   = DelimiterAdaptor(s, BoolDelimiter().FromLuaStr)
     null_generator   = DelimiterAdaptor(s, NullDelimiter().FromLuaStr)
+    # comment_generator= DelimiterAdaptor(s, CommentDelimiter().FromLuaStr)
     list_generator   = DelimiterAdaptor(s, ListDelimiter().FromLuaStr)
     dict_generator   = DelimiterAdaptor(s, DictDelimiter().FromLuaStr)
-    return Generator(number_generator, string_generator, bool_generator,null_generator, list_generator, dict_generator)
+    pos, v = Generator(
+        number_generator, 
+        string_generator, 
+        bool_generator,
+        null_generator, 
+        # comment_generator,
+        list_generator, 
+        dict_generator)
+    return pos + p, v
 
 
-# def LuaStrToStruct(s):
-#     empty = None
-#     result = None
-#     number_generator = Generator(s, NumberDelimiter())
-#     string_generator = Generator(s, StringDelimiter())
 
 class PyLuaTblParser:
     '''load(self, s) '''
     def __init__(self):
         self.__data = None
 
-    def load(self, s):
-        global source_input
-        source_input = s
+    def load(self, s = ""):
+        # s = ("%r" % s).replace('"',"\\")
+        # s = s.replace("\"", '\\"')
+        # s.encode('string-escape')
         self.__data = LuaStrToStruct(s)[1]
 
     def dump(self):
--- track_map_creator.py
+++ track_map_creator.py
@@ -1,6 +1,7 @@
 
 def create_track(outfile):
-    ft = '''def __ascii_%d(s, d=100):\n if d ==0 or len(s) == 0: raise Exception("") \n else: track_map[ord(s[0])](s[1:], d - 1)\n'''
+    outfile.write("from common import *\n")
+    ft = '''def __ascii_%d(s, d=100):\n if d ==0 or len(s) == 0: raise NonePattern("") \n else: track_map[ord(s[0])](s[1:], d - 1)\n'''
     for i in range(0, 255):
         outfile.write(ft % i)
 
@@ -14,12 +15,13 @@ def create_map(outfile):
     outfile.write(ft)
 
 # with open("track_map.py","w") as fd:
-    # create_track(fd)
-    # create_map(fd)
+#     create_track(fd)
+#     create_map(fd)
 
 # import track_map
 # track_map.ascii("abc")
 # l = [345,336,336,351,99,186,99,372,42,33,30,335]
+
 def map_to_str(l = []):
     s = ""
     if len(l) == 0:
@@ -27,112 +29,18 @@ def map_to_str(l = []):
     else:
         l[len(l) - 1] += 1
     for c in l:
-        s += chr(c/3-1)
+        if c >= 3:
+            s += chr(c/3-1)
     print s
 
 import re
-def tract_to_list(s = ''):
+def track_to_list(s = ''):
     return [int(i) for i in re.findall(r'\d+', s)]
-l = tract_to_list('''  File "track_map.py", line 372
-  File "track_map.py", line 42
-  File "track_map.py", line 33
-  File "track_map.py", line 345
-  File "track_map.py", line 336
-  File "track_map.py", line 336
-  File "track_map.py", line 351
-  File "track_map.py", line 99
-  File "track_map.py", line 186
-  File "track_map.py", line 99
-  File "track_map.py", line 372
-  File "track_map.py", line 42
-  File "track_map.py", line 33
-  File "track_map.py", line 30
-  File "track_map.py", line 105
-  File "track_map.py", line 255
-  File "track_map.py", line 306
-  File "track_map.py", line 348
-  File "track_map.py", line 351
-  File "track_map.py", line 99
-  File "track_map.py", line 243
-  File "track_map.py", line 294
-  File "track_map.py", line 351
-  File "track_map.py", line 351
-  File "track_map.py", line 306
-  File "track_map.py", line 345
-  File "track_map.py", line 333
-  File "track_map.py", line 99
-  File "track_map.py", line 252
-  File "track_map.py", line 351
-  File "track_map.py", line 345
-  File "track_map.py", line 318
-  File "track_map.py", line 333
-  File "track_map.py", line 312
-  File "track_map.py", line 105
-  File "track_map.py", line 135
-  File "track_map.py", line 42
-  File "track_map.py", line 33
-  File "track_map.py", line 30
-  File "track_map.py", line 138
-  File "track_map.py", line 138
-  File "track_map.py", line 99
-  File "track_map.py", line 372
-  File "track_map.py", line 105
-  File "track_map.py", line 336
-  File "track_map.py", line 297
-  File "track_map.py", line 321
-  File "track_map.py", line 306
-  File "track_map.py", line 300
-  File "track_map.py", line 351
-  File "track_map.py", line 99
-  File "track_map.py", line 360
-  File "track_map.py", line 318
-  File "track_map.py", line 351
-  File "track_map.py", line 315
-  File "track_map.py", line 99
-  File "track_map.py", line 150
-  File "track_map.py", line 99
-  File "track_map.py", line 330
-  File "track_map.py", line 306
-  File "track_map.py", line 330
-  File "track_map.py", line 297
-  File "track_map.py", line 306
-  File "track_map.py", line 345
-  File "track_map.py", line 105
-  File "track_map.py", line 99
-  File "track_map.py", line 186
-  File "track_map.py", line 99
-  File "track_map.py", line 372
-  File "track_map.py", line 105
-  File "track_map.py", line 294
-  File "track_map.py", line 345
-  File "track_map.py", line 345
-  File "track_map.py", line 294
-  File "track_map.py", line 366
-  File "track_map.py", line 99
-  File "track_map.py", line 360
-  File "track_map.py", line 318
-  File "track_map.py", line 351
-  File "track_map.py", line 315
-  File "track_map.py", line 99
-  File "track_map.py", line 150
-  File "track_map.py", line 99
-  File "track_map.py", line 306
-  File "track_map.py", line 327
-  File "track_map.py", line 306
-  File "track_map.py", line 330
-  File "track_map.py", line 306
-  File "track_map.py", line 333
-  File "track_map.py", line 351
-  File "track_map.py", line 105
-  File "track_map.py", line 135
-  File "track_map.py", line 378
-  File "track_map.py", line 135
-  File "track_map.py", line 378
-  File "track_map.py", line 135
-  File "track_map.py", line 42
-  File "track_map.py", line 33
-  File "track_map.py", line 30
-  File "track_map.py", line 371
 
-''')
-map_to_str(l)
+
+with open("track_data.txt","r") as fd:
+    s = fd.read()
+    l = track_to_list(s)
+    ma = max(l)
+    mi = min(l)
+    map_to_str(l)
